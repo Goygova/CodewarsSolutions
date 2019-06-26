db94594b08eaf608de62c3bf09a7017e2c48a732
Anastasia Goygova <goygova@gmail.com>
2019-06-25 17:30:59 -0700

    Updated Kata Solutions. Added new Kata Solution.

--- KataSolutions.js
+++ KataSolutions.js
@@ -4,3 +4,15 @@ https://www.codewars.com/kata/541c8630095125aba6000c00
 function digital_root(n) {
   return (n - 1) % 9 + 1;
 }
+
+
+/* 7 kyu Mumbling / Solution 
+https://www.codewars.com/kata/5667e8f4e3f572a8f2000039 */
+
+function accum(s) {;
+  let newStr = '';
+  for(let i = 0; i < s.length; i++){
+     newStr += s[i].toUpperCase() + s[i].repeat(i).toLowerCase() + '-';
+  }
+ return newStr.substring(0, newStr.length-1);
+}
