ea1d51625872ec0dd8f6c96ff65f82c678acaebc
Anastasia Goygova <goygova@gmail.com>
2019-08-07 19:49:26 -0700

    Added new Kata Solution gor Java

--- KataSolutions.java
+++ KataSolutions.java
@@ -224,3 +224,17 @@ public class Block{
     return 2 * (width * length + width * height + length * height);
   }  
 }
+//7 kyu Two fighters, one winner.
+//https://www.codewars.com/kata/577bd8d4ae2807c64b00045b
+
+public class Kata {
+  public static String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
+    while(true){
+      fighter1.health -= fighter2.damagePerAttack;
+      fighter2.health -= fighter1.damagePerAttack;
+      
+      if(fighter1.health <= 0 && fighter2.health <=0) return firstAttacker;
+      if(fighter1.health <= 0)return fighter2.name;
+      if(fighter2.health <= 0)return fighter1.name;
+      }
+  }
